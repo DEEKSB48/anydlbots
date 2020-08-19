6197b5f58d540888ccda9a3f0d84768d0fe47e2a
ikssas35 <68480914+ikssas35@users.noreply.github.com>
2020-08-19 22:43:45 +0600

    Update and rename sample_config.py to config.py

--- anydlbot/config.py
+++ anydlbot/config.py
@@ -5,8 +5,8 @@ class Config(object):
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
